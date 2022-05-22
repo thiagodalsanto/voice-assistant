cddacdd90d43af013f1139bb5ffbaa928fa7577d
Thiago Dal Santo <thiagodalsantodaluz@gmail.com>
2022-05-22 19:29:22 -0300

    add react-native

---
+++ SourceCode/src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => (
+  <View style={{ 
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center"  }}> 
+    <Text>Thiago</Text>
+  </View>
+);
+
+export default App;
